8a5fa2df43c52bd30d909e8bb763c6efd2dce1cf
Ildar <shaikhivaliev@gmail.com>
2020-04-14 01:06:36 +0300

    add network request with coroutines

--- app/src/main/java/com/petapp/capybara/MainActivity.kt
+++ app/src/main/java/com/petapp/capybara/MainActivity.kt
@@ -2,11 +2,33 @@ package com.petapp.capybara
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        const val id = 744
+    }
+
+    lateinit var viewModel: DatasetViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        viewModel = ViewModelProvider(this).get(DatasetViewModel::class.java)
+        viewModel.getData(id)
+        initObservers()
+    }
+
+    private fun initObservers() {
+        viewModel.inProgress.observe(this, Observer {
+            if (it) toast("Progress...") else toast("Done")
+        })
+        viewModel.data.observe(this, Observer {
+            showSnackMessage(it.departmentCaption)
+        })
+        viewModel.error.observe(this, Observer {
+            showSnackMessage(it)
+        })
     }
 }
