751677ac3d2f77e2444295b9a2fcdf9446ecc93f
Ildar <shaikhivaliev@gmail.com>
2020-06-15 13:22:38 +0300

    refactoring ProfileCustomView.kt after PR home_task_5

--- app/src/main/java/com/petapp/capybara/profiles/presentation/view/ProfileCustomView.kt
+++ app/src/main/java/com/petapp/capybara/profiles/presentation/view/ProfileCustomView.kt
@@ -13,21 +13,25 @@ import com.petapp.capybara.R
 import com.petapp.capybara.extensions.dpTpPx
 
 class ProfileCustomView @JvmOverloads constructor(
-        context: Context,
-        attributeSet: AttributeSet? = null,
-        defStyleAttr: Int = 0
-    ) : AppCompatImageView(context, attributeSet, defStyleAttr) {
+    context: Context,
+    attributeSet: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : AppCompatImageView(context, attributeSet, defStyleAttr) {
 
     companion object {
-        private const val DEFAULT_SIZE = 56
-        private const val DEFAULT_BORDER_WIDTH = 6
+        private const val DEFAULT_SIZE_DP = 56
+        private const val DEFAULT_BORDER_WIDTH_DP = 6
+        private const val TAG = "custom_view"
     }
 
     private val avatarPaint = Paint(Paint.ANTI_ALIAS_FLAG)
     private val borderPaint = Paint(Paint.ANTI_ALIAS_FLAG)
     private val initialsPaint = Paint(Paint.ANTI_ALIAS_FLAG)
-    private val rect = Rect()
+    // главная область рисования
+    private val viewRect = Rect()
+    // для рисования каемки
     private var borderRect = Rect()
+    // рисуем либо инициалы, либо изображение (isAvatarMode = true)
     private var isAvatarMode = true
     private var initials = ""
 
@@ -38,7 +42,7 @@ class ProfileCustomView @JvmOverloads constructor(
 
     private fun setup() {
         with(borderPaint) {
-            strokeWidth = context.dpTpPx(DEFAULT_BORDER_WIDTH)
+            strokeWidth = context.dpTpPx(DEFAULT_BORDER_WIDTH_DP)
             color = Color.WHITE
             style = Paint.Style.STROKE
         }
@@ -47,42 +51,46 @@ class ProfileCustomView @JvmOverloads constructor(
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         when (MeasureSpec.getMode(widthMeasureSpec)) {
             MeasureSpec.UNSPECIFIED -> {
-                val defaultSize = context.dpTpPx(DEFAULT_SIZE).toInt()
+                val defaultSize = context.dpTpPx(DEFAULT_SIZE_DP).toInt()
                 setMeasuredDimension(defaultSize, defaultSize)
-                Log.d("custom_view", "onMeasure UNSPECIFIED")
+                Log.d(TAG, "onMeasure UNSPECIFIED")
             }
             MeasureSpec.AT_MOST -> {
                 setMeasuredDimension(widthMeasureSpec, heightMeasureSpec)
-                Log.d("custom_view", "onMeasure AT_MOST")
+                Log.d(TAG, "onMeasure AT_MOST")
             }
             MeasureSpec.EXACTLY -> {
                 setMeasuredDimension(widthMeasureSpec, heightMeasureSpec)
-                Log.d("custom_view", "onMeasure EXACTLY")
+                Log.d(TAG, "onMeasure EXACTLY")
             }
         }
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
-        Log.d("custom_view", "onSizeChanged")
+        Log.d(TAG, "onSizeChanged")
         if (w == 0) return
-        with(rect) {
+        with(viewRect) {
             left = 0
             top = 0
             right = w
             bottom = h
         }
-        borderRect = rect.apply {
-            val half = (context.dpTpPx(DEFAULT_BORDER_WIDTH) / 2).toInt()
+        borderRect = viewRect.apply {
+            val half = (context.dpTpPx(DEFAULT_BORDER_WIDTH_DP) / 2).toInt()
             this.inset(half, half)
         }
         prepareShader(w, h)
     }
 
     override fun onDraw(canvas: Canvas) {
-        if (drawable != null && isAvatarMode) drawAvatar(canvas) else drawInitials(canvas)
+        if (drawable != null && isAvatarMode) {
+            drawAvatar(canvas)
+        } else {
+            drawInitials(canvas)
+        }
         canvas.drawOval(borderRect.toRectF(), borderPaint)
-        Log.d("custom_view", "onDraw")
+        Log.d(TAG, "onDraw")
         super.dispatchDraw(canvas)
     }
 
@@ -93,20 +101,20 @@ class ProfileCustomView @JvmOverloads constructor(
     }
 
     private fun drawAvatar(canvas: Canvas) {
-        canvas.drawOval(rect.toRectF(), avatarPaint)
+        canvas.drawOval(viewRect.toRectF(), avatarPaint)
     }
 
     private fun drawInitials(canvas: Canvas) {
         initialsPaint.color = ContextCompat.getColor(context, R.color.light_gray)
-        canvas.drawOval(rect.toRectF(), initialsPaint)
+        canvas.drawOval(viewRect.toRectF(), initialsPaint)
         with(initialsPaint) {
             color = ContextCompat.getColor(context, R.color.light_black)
             textAlign = Paint.Align.CENTER
             textSize = height * 0.33F
         }
         val offsetY = (initialsPaint.descent() + initialsPaint.ascent()) / 2
-        canvas.drawText(initials, rect.exactCenterX(), rect.exactCenterY() - offsetY, initialsPaint)
-        Log.d("custom_view", "drawInitials")
+        canvas.drawText(initials, viewRect.exactCenterX(), viewRect.exactCenterY() - offsetY, initialsPaint)
+        Log.d(TAG, "drawInitials")
     }
 
     fun setColor(color: Int) {
@@ -122,18 +130,18 @@ class ProfileCustomView @JvmOverloads constructor(
     override fun setImageBitmap(bm: Bitmap?) {
         super.setImageBitmap(bm)
         if (isAvatarMode) prepareShader(width, height)
-        Log.d("custom_view", "setImageBitmap")
+        Log.d(TAG, "setImageBitmap")
     }
 
     override fun setImageDrawable(drawable: Drawable?) {
         super.setImageDrawable(drawable)
         if (isAvatarMode) prepareShader(width, height)
-        Log.d("custom_view", "setImageDrawable")
+        Log.d(TAG, "setImageDrawable")
     }
 
     override fun setImageResource(resId: Int) {
         super.setImageResource(resId)
         if (isAvatarMode) prepareShader(width, height)
-        Log.d("custom_view", "setImageResource")
+        Log.d(TAG, "setImageResource")
     }
 }
